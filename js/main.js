a8d94ad672ac872ece69b6df64a7be5cbb1e7629
V3Vanity <jun30010kmtn.ruu@gmail.com>
2021-07-16 00:03:40 +0300

    create: slider reviews

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".hotel-slider", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
@@ -8,6 +8,22 @@ const swiper = new Swiper(".hotel-slider", {
     prevEl: ".hotel-slider__button--prev",
   },
   effect: "coverflow",
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const reviewSlaider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
